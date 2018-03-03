9852bdb4c697345a59d330de229f143956ba37e4
cecpk <cec@retrospot.de>
2018-03-03 10:04:00 +0100

    Add getPokemonRarity_noi8 function

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -1163,6 +1163,14 @@ function getPokemonRarity(pokemonId) {
     return ''
 }
 
+function getPokemonRarity_noi8(pokemonId) {
+    if (pokemonRarities.hasOwnProperty(pokemonId)) {
+        return pokemonRarities[pokemonId]
+    }
+
+    return ''
+}
+
 function getGoogleSprite(index, sprite, displayHeight) {
     displayHeight = Math.max(displayHeight, 3)
     var scale = displayHeight / sprite.iconHeight
@@ -1205,7 +1213,7 @@ function setupPokemonMarkerDetails(item, map, scaleByRarity = true, isNotifyPkmn
             'legendary': 50
         }
 
-        const pokemonRarity = getPokemonRarity(item['pokemon_id']).toLowerCase()
+        const pokemonRarity = getPokemonRarity_noi8(item['pokemon_id']).toLowerCase()
         if (rarityValues.hasOwnProperty(pokemonRarity)) {
             rarityValue = rarityValues[pokemonRarity] 
             
