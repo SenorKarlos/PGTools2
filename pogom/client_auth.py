fdd9e2cba302d8022113825e816a4c56480fcada
Till Skrodzki <till@mueskro.de>
2018-04-08 12:55:30 +0200

    Initial PR that's to be reworked

--- pogom/client_auth.py
+++ pogom/client_auth.py
@@ -6,107 +6,102 @@ import requests
 import urllib
 import datetime
 
-from flask import jsonify
+from flask import redirect
 from requests.exceptions import HTTPError
 
 log = logging.getLogger(__name__)
-log.setLevel('INFO')
 
-
-def check_auth(args, request, user_auth_code_cache):
+def check_auth(args, url_root, session, user_auth_code_cache):
     if args.user_auth_service == "Discord":
         host = args.uas_host_override
         if not host:
-            host = request.url_root
-        if not valid_client_auth(request, host, user_auth_code_cache, args):
+            host = url_root
+        if not valid_client_auth(host, session, user_auth_code_cache, args):
             return redirect_client_to_auth(host, args)
         if args.uas_discord_required_guilds:
-            if not valid_discord_guild(request, user_auth_code_cache, args):
+            if not valid_discord_guild(session, user_auth_code_cache, args):
+                log.info('User is not in guild, redirecting...')
                 return redirect_to_discord_guild_invite(args)
-            if (args.uas_discord_required_roles
-                and not valid_discord_guild_role(
-                    request, user_auth_code_cache, args)):
+            if args.uas_discord_required_roles
+            and not valid_discord_guild_role(session,
+            user_auth_code_cache, args):
+                log.info('User doesnot have required role, redirecting...')
                 return redirect_to_discord_guild_invite(args)
-    return False
+        return None
+
+
+
+
 
 
 def redirect_client_to_auth(host, args):
-    d = {}
-    d['auth_redirect'] = (
-        'https://discordapp.com/api/oauth2/authorize?client_id=' +
-        args.uas_client_id + '&redirect_uri=' +
-        urllib.quote(host + 'auth_callback') +
-        '&response_type=code&scope=identify%20guilds')
-    return jsonify(d)
+    return redirect('https://discordapp.com/api/oauth2/authorize?client_id=' + args.uas_client_id + '&redirect_uri=' + urllib.quote(host + 'auth_callback') + '&response_type=code&scope=identify%20guilds')
 
-
-def valid_client_auth(request, host, user_auth_code_cache, args):
-    userAuthCode = request.args.get('userAuthCode')
+def valid_client_auth(host, session, user_auth_code_cache, args):
+    userAuthCode = session.get('userAuthCode')
     if not userAuthCode:
-        log.debug("no userAuthCode")
+        log.info('No userAuthCode')
         return False
     oauth_response = user_auth_code_cache.get(userAuthCode, False)
     if not oauth_response:
         oauth_response = exchange_code(userAuthCode, host, args)
-        if (not oauth_response):
+        if not oauth_response:
+            log.debug("Unable to authorize code")
             return False
         user_auth_code_cache[userAuthCode] = oauth_response
-        user_auth_code_cache[userAuthCode]['expires'] = (
-            datetime.datetime.now() +
-            datetime.timedelta(0, int(oauth_response.get('expires_in'))))
+        user_auth_code_cache[userAuthCode]['expires'] = datetime.datetime.now() + datetime.timedelta(0, int(oauth_response.get('expires_in')))
     if user_auth_code_cache[userAuthCode]['expires'] < datetime.datetime.now():
-        log.debug("oauth expired")
+        log.debug("Oauth expired")
         del user_auth_code_cache[userAuthCode]
         return False
     if args.uas_discord_required_guilds:
         if not user_auth_code_cache[userAuthCode].get('guilds', False):
-            user_auth_code_cache[userAuthCode]['guilds'] = get_user_guilds(
-                user_auth_code_cache[userAuthCode]['access_token'])
+            user_auth_code_cache[userAuthCode]['guilds'] = get_user_guilds(user_auth_code_cache[userAuthCode]['access_token'])
             if not user_auth_code_cache[userAuthCode]['guilds']:
+                log.debug("Unable to access user guilds.")
                 del user_auth_code_cache[userAuthCode]
                 return False
         if args.uas_discord_required_roles:
             if not user_auth_code_cache[userAuthCode].get('roles', False):
-                user_auth_code_cache[userAuthCode]['roles'] = (
-                    get_user_guild_roles(
-                        user_auth_code_cache[userAuthCode]['access_token'],
-                        args))
+                user_auth_code_cache[userAuthCode]['roles'] = get_user_guild_roles(user_auth_code_cache[userAuthCode]['access_token'], args)
                 if not user_auth_code_cache[userAuthCode]['roles']:
+                    log.debug("Unable to access user roles.")
                     del user_auth_code_cache[userAuthCode]
                     return False
-    return True
+  return True
 
 
-def valid_discord_guild(request, user_auth_code_cache, args):
-    userAuthCode = request.args.get('userAuthCode')
-    guilds = user_auth_code_cache.get(userAuthCode)['guilds']
-    required_guilds = (
-        [x.strip() for x in args.uas_discord_required_guilds.split(',')])
-    for g in guilds:
-        if g['id'] in required_guilds:
-            return True
-    log.debug("User not in required discord guild.")
-    del user_auth_code_cache[userAuthCode]['guilds']
-    return False
+def valid_discord_guild(session, user_auth_code_cache, args):
+  userAuthCode = session.get('userAuthCode')
+  guilds = user_auth_code_cache.get(userAuthCode, {}).get('guilds')
+  required_guilds = [x.strip() for x in args.uas_discord_required_guilds.split(',')]
+  for g in guilds:
+    if g['id'] in required_guilds:
+      return True
+  log.debug("User not in required discord guild.")
+  del user_auth_code_cache[userAuthCode]['guilds']
+  return False
 
 
-def valid_discord_guild_role(request, user_auth_code_cache, args):
-    userAuthCode = request.args.get('userAuthCode')
-    userRoles = user_auth_code_cache.get(userAuthCode)['roles']
-    requiredRoles = (
-        [x.strip() for x in args.uas_discord_required_roles.split(',')])
-    for r in userRoles:
-        if r in requiredRoles:
-            return True
-    log.debug("User not in required discord guild role.")
-    del user_auth_code_cache[userAuthCode]['roles']
-    return False
+def valid_discord_guild_role(session, user_auth_code_cache, args):
+  userAuthCode = session.get('userAuthCode')
+  userRoles = user_auth_code_cache.get(userAuthCode, {}).get('roles')
+  requiredRoles = [x.strip() for x in args.uas_discord_required_roles.split(',')]
+  for r in userRoles:
+    if r in requiredRoles:
+      return True
+  log.debug("User not in required discord guild role.")
+  del user_auth_code_cache[userAuthCode]['roles']
+  return False
+
+
+
+
+
 
 
 def redirect_to_discord_guild_invite(args):
-    d = {}
-    d['auth_redirect'] = args.uas_discord_guild_invite
-    return jsonify(d)
+    return redirect(args.uas_discord_guild_invite)
 
 
 def exchange_code(code, host, args):
